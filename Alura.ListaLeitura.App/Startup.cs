369ca934557b423c47c12c9fdeddb179b6eb1138
Pedrinho <plluz@outlook.com>
2020-04-25 17:03:13 -0300

    início do roteamento de requests com dictionary

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -10,13 +11,27 @@ namespace Alura.ListaLeitura.App
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(MostrarLivrosParaLer);
+            app.Run(Roteamento);
         }
 
-        public Task MostrarLivrosParaLer(HttpContext context)
+        public Task Roteamento(HttpContext context)
         {
-            var _repo = new LivroRepositorioCSV();
-            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+            var repo = new LivroRepositorioCSV();
+
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                {"/Livros/ParaLer", repo.ParaLer.ToString() },
+                {"/Livros/Lendo", repo.Lendo.ToString() },
+                {"/Livros/Lidos", repo.Lidos.ToString() }
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync($"Caminho '{context.Request.Path}' inexistente!");
         }
     }
 }
